188c70333e6d5aa3287b8326c7e3f4af73403580
Mihir Kuma Singh <mihir39nr@gmail.com>
2023-11-13 21:34:45 +0530

    Added error.js

---
+++ src/error.rs
@@ -0,0 +1,60 @@
+use serde::Serialize;
+use thiserror::Error;
+use std::convert::Infallible;
+use warp::{http::StatusCode,Rejection, Reply, reply::{Json, self}};
+use mongodb::bson;
+
+#[derive(Error,Debug)]
+pub enum Error {
+    #[error("mongodb error:{0}")]
+    MongoError(#[from] mongodb::error::Error),
+    #[error("error during mongodb query: {0}")]
+    MongoQueryError(mongodb::error::Error),
+    #[error("could not access field in document: {0}")]
+    MongoDataError(#[from] bson::document::ValueAccessError),
+    #[error("invalid id used: {0}")]
+    InvalidIDError(String)
+}
+
+#[derive(Serialize)]
+struct ErrorResponse{
+    message: String,
+}
+
+impl warp::reject::Reject for Error {}
+
+pub async fn handle_rejection(err: Rejection) -> std::result::Result<Box<dyn Reply>, Infallible>{
+    let code: StatusCode;
+    let message: &str;
+
+    if err.is_not_found() {
+        code = StatusCode::NOT_FOUND;
+        message = "Not Found";
+    }
+    else if let Some(_) = err.find::<warp::filters::body::BodyDeserializeError>(){
+        code = StatusCode::BAD_REQUEST;
+        message = "Invalid Body";
+    }
+    else if let Some(e) = err.find::<Error>(){
+        match e{
+            _ => {
+                eprintln!("unhandled application error: {:?}",err);
+                code = StatusCode::INTERNAL_SERVER_ERROR;
+                message= "Internal Server Error";
+            }
+        }
+    }
+    else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
+        code= StatusCode::METHOD_NOT_ALLOWED;
+        message = "Method not found";
+    }
+    else {
+        eprintln!("unhandled Error {:?}",err);
+        code = StatusCode::INTERNAL_SERVER_ERROR;
+        message= "Internal Server Error";
+    }
+    let json: Json = reply::json(&ErrorResponse{
+        message: message.into(),
+    });
+    Ok(Box::new(reply::with_status(json, code)))
+}
